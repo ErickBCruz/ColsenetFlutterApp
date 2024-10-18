276c43111485a49d994dde99b024af3cca8677ee
ErickBCruz <erickbaezcruz@gmail.com>
2024-10-18 15:48:46 -0500

    add folding menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:colsenet/Screens/login.dart';
 import 'package:flutter/material.dart';
-import 'Screens/login.dart';
+//import 'Screens/login.dart';
+import 'Screens/home_map.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import '../Themes/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +16,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Colsenet",
-      home: Login(),
+      home: Inicio(),
     );
   }
 }
@@ -29,56 +33,50 @@ class _InicioState extends State<Inicio> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("text"),
+        backgroundColor: AppColor.primary,
+        title: const Text('Colsenet', style: TextStyle(color: AppColor.secundary)),
       ),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: Image.network("https://staticfiles.warthunder.com/upload/image/0_2024/7_July/F14/news_main_f14a_iriaf_mar_dfd82b383a03bee7b31a833164657dbc.jpg")
-          ),
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: Image.network("https://staticfiles.warthunder.com/upload/image/0_2024/7_July/F14/news_main_f14a_iriaf_mar_dfd82b383a03bee7b31a833164657dbc.jpg"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: Image.network("https://staticfiles.warthunder.com/upload/image/0_2024/7_July/F14/news_main_f14a_iriaf_mar_dfd82b383a03bee7b31a833164657dbc.jpg"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: const ElevatedButton(onPressed: null, child: Text("Elevated"))
-          ),
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: const FilledButton(onPressed: null, child: Text("Filled"))
-          ),
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: const OutlinedButton(onPressed: null, child: Text("Outline"))
-          ),
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: const TextButton(onPressed: null, child: Text("Text"))
-          ),
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: ElevatedButton.icon(onPressed: (){}, icon: const Icon(Icons.add_moderator), label: const Text("variacion"),)
-          ),
-          cuerpo(),
+      body: const Stack(
+        children: [
+          MapPage()
         ],
-      )
+      ),
+      floatingActionButton: SpeedDial(
+        icon: Icons.menu,
+        backgroundColor: AppColor.secundary,
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.person),
+            label: 'Ingresar',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Login()),
+              );
+            },
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.person_add),
+            label: 'Registro',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Login()),
+              );
+            },
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.info),
+            label: 'Ayuda',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Login()),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
-}
-
-Widget cuerpo(){
-  return Container(
-    decoration: const BoxDecoration(
-      image: DecorationImage(image: NetworkImage("https://staticfiles.warthunder.com/upload/image/0_2024/7_July/F14/news_main_f14a_iriaf_mar_dfd82b383a03bee7b31a833164657dbc.jpg"),
-      fit: BoxFit.cover,
-      )
-    ),
-    child: const Text("hola"),
-  );
 }
